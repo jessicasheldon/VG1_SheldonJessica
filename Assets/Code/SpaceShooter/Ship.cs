baa64a45dcec410038b1f4c0b0ec7784ea83dd37
jessicasheldon <jessicasheldon@wustl.edu>
2024-03-25 15:47:12 -0500

    Q5 Progress

--- Assets/Code/SpaceShooter/Ship.cs
+++ Assets/Code/SpaceShooter/Ship.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SpaceShooter
 {
@@ -8,9 +9,12 @@ namespace SpaceShooter
     {
         // Outlets
         public GameObject projectilePrefab;
+        public Image imageHealthBar;
 
         // State tracking
         public float firingDelay = 1f;
+        public float healthMax = 100f;
+        public float health = 100f;
 
         void Start()
         {
@@ -32,13 +36,45 @@ namespace SpaceShooter
         // Methods
         void Update()
         {
-            float yPosition = Mathf.Sin(GameController.instance.timeElapsed) * 3f;
-            transform.position = new Vector2(0, yPosition);
+            if (health > 0)
+            {
+                float yPosition = Mathf.Sin(GameController.instance.timeElapsed) * 3f;
+                transform.position = new Vector2(0, yPosition);
+            }
+            
         }
 
         void FireProjectile()
         {
             Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.GetComponent<Asteroid>())
+            {
+                TakeDamage(10f);
+            }
+        }
+
+        void TakeDamage(float damageAmount)
+        {
+            health -= damageAmount;
+
+            if (health <= 0)
+            {
+                Die();
+            }
+
+            imageHealthBar.fillAmount = health / healthMax;
+        }
+
+        void Die()
+        {
+            StopCoroutine("FiringTimer");
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            rb.bodyType = RigidbodyType2D.Dynamic;
         }
     }
 }
